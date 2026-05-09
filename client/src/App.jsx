da440ff078eaab8fdbf3227e04f428cb7c5ca5ce
anapao07 <annapao.arcos@gmail.com>
2026-05-09 15:56:24 -0600

    React Initial Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ProductsPage } from './pages/ProductsPage';
+import { ProductsFormPage } from './pages/ProductsFormPage';
 
-function App(){
-  return
-  (
-    <div>Hello Word</div>
-  )
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products-create" element={<ProductsFormPage />} />
+        <Route path="*" element={<div>404 - Página no encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-export default App
+
+export default App;
---
+++ client/src/pages/ProductsFormPage.jsx
@@ -0,0 +1,5 @@
+export function ProductsFormPage(){
+    return (
+        <div> Products Form Page</div>
+    )
+}

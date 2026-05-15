a1a6c96e75473f8019dd99204cb70daaadffa247
anapao07 <annapao.arcos@gmail.com>
2026-05-15 14:10:24 -0600

    :art: feat: Category field added to model

--- products/models.py
+++ products/models.py
@@ -15,6 +15,12 @@ class Status(models.Model):
     def __str__(self):
         return self.name
 
+
+class Category(models.Model):
+    name = models.CharField(_("Nombre del Estado"), max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+
 class Products(models.Model):
     name = models.CharField(_("Nombre"), max_length=200)
     description = models.TextField(_("Descripción"), blank=True)
@@ -35,6 +41,12 @@ class Products(models.Model):
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        verbose_name=_("Categoria")
+    )
 
     def __str__(self):
         return self.name
